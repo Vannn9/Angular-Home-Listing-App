878e006e80dbdffc6a58d2522382aebed187a499
Brandon Van <brandontvan0@gmail.com>
2024-05-02 10:30:21 -0500

    commit full app

---
+++ first-app/src/app/details/details.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  
+  constructor() {
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
+  }
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
+  }
+
+}
